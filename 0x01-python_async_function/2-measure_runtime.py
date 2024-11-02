56678072ee3f6db61ccaf333604fa91655bdd7f7
AbanoubAziz <abanoubsamy2341@gmail.com>
2024-11-02 21:10:22 +0200

    task 3 done

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -5,7 +5,7 @@ Basic async syntax
 import asyncio
 import time
 
-
+    
 wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
---
+++ 0x01-python_async_function/3-tasks.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Tasks"""
+import asyncio
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+def task_wait_random(max_delay: int) -> asyncio.Task:
+    """Wait for a random delay between 0 and max_delay"""
+    return asyncio.Task(wait_random(max_delay))
