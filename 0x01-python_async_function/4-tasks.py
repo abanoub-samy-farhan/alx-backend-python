5912b32f1d21dc3eb54b1052322fd0104c9067a1
AbanoubAziz <abanoubsamy2341@gmail.com>
2024-11-02 21:23:12 +0200

    task 4 annotation fixed

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 """Tasks"""
 import asyncio
+from typing import List
 
 task_wait_random = __import__('3-tasks').task_wait_random
 
 
-async def task_wait_n(n: int, max_delay: int) -> float:
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """Wait for a random delay between 0 and max_delay"""
     delays = []
 
