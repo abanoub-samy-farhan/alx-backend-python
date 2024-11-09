cecb05734b3eddc240b72232961ababf43f20c1a
AbanoubAziz <abanoubsamy2341@gmail.com>
2024-11-09 03:09:43 +0200

    Async 1

---
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+Training on making some Async comperhension tasks
+"""
+
+
+import asyncio
+import random
+from typing import List
+
+
+async_generator = __import__("0-async_generator").async_generator
+
+
+async def async_comprehension() -> List[float]:
+    """Async comperhension function that return the ready list"""
+    lst = []
+    async for i in async_generator():
+        lst.append(i)
+    return lst
