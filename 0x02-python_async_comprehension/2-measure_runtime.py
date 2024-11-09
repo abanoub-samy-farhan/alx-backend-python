e5e2547a5e63294f9d0aecd17ec6a8c4c748aba1
AbanoubAziz <abanoubsamy2341@gmail.com>
2024-11-09 03:14:40 +0200

    Asycn 1

---
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+Training on making some Async comperhension tasks
+"""
+
+
+import asyncio
+import random
+import time
+
+
+async_comprehension = __import__("1-async_comprehension").async_comprehension
+
+async def measure_runtime() -> float:
+    """Function time that function take to process"""
+    start = time.time()
+    lst = await async_comprehension()
+    elapsed = time.time() - start
+    return elapsed
