52e13a867f6b9c7ba25f0bd16f010d3b065435d6
AbanoubAziz <abanoubsamy2341@gmail.com>
2024-11-09 03:20:40 +0200

    List comprehension added

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -14,7 +14,5 @@ async_generator = __import__("0-async_generator").async_generator
 
 async def async_comprehension() -> List[float]:
     """Async comperhension function that return the ready list"""
-    lst = []
-    async for i in async_generator():
-        lst.append(i)
+    lst = [i async for i in aiter(async_generator())]
     return lst
