4fd7d35e4988b791c8ae3ebf8f12f4da470a45b5
AbanoubAziz <abanoubsamy2341@gmail.com>
2024-11-09 03:27:35 +0200

    Async 2

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -14,6 +14,7 @@ async_comprehension = __import__("1-async_comprehension").async_comprehension
 async def measure_runtime() -> float:
     """Function time that function take to process"""
     start = time.time()
-    lst = await async_comprehension()
+    await asyncio.gather(async_comprehension(), async_comprehension(),
+                   async_comprehension(), async_comprehension())
     elapsed = time.time() - start
     return elapsed
